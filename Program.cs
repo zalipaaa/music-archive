f9d3fabd02276337aac80084bd835a3b978388f8
zalipaaa <zxceloabuze@gmail.com>
2026-06-08 09:06:20 +0300

    Final fix: Add all missing using directives (System.IO, System, etc.)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using System;
+using System.Text;
 using MusicArchive.Services;
 using MusicArchive.UI;
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
 
 var repository = new SongRepository();
 var menu = new Menu(repository);
